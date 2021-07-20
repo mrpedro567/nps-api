bfaf9ad2f73bebb6d27bb8f1926304ffd4ef0d70
Pedro Moraes <pmoraes@asert.com.br>
2021-07-20 20:32:57 -0300

    Tag controller inital commit

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import TagController from '../controllers/tag.controller';
+import connection from '../config/connection';
 
 export const routes = express.Router();
+const tagController = new TagController(connection);
 
 routes.get('/', (req, res) => {
     res.status(200).send('Level5Jr Nps collector API')
-})
+});
+
+routes.get('/tag', (req, res) => tagController.getTag(req, res));
 
 module.exports = routes;
